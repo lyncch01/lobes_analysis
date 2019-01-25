851c0eb34d647fa78c8d9a7738a1f9409c84c9d5
lyncch01 <lyncch01@gmail.com>
2019-01-25 12:53:55 +0800

    updated comments to be correct

--- regrid_images.py
+++ regrid_images.py
@@ -1,46 +1,37 @@
 '''
-Program runs within CASA environment and does the following: (1) create sub-images centred 
-on targeted sources (coord read from cvs file); (2) fit 2min sub-images; (3) median stack 
-images on specified time-scales for all sources; (4) fit median-stacked images; 
-(5) create summary of all >4 sigma detections (median stack + snap shot)
+Python program that reads in all images and then runs mirRegrid (miriad script) to regrid the images to match the first image (time-ordered). 
 '''
-
-import numpy as np
 import glob
-from astropy.io import fits
-from astropy import units as au
-from astropy.coordinates import SkyCoord
-from astropy.coordinates import Angle
 import os
-import gc
-import csv
-
-
-
 
 #######################################################################################
-#								 MAIN PROGRAM       
+## MAIN PROGRAM       
 #######################################################################################
 
-#os.system("mkdir Images")
+##Read in the 2-min images and sort by name (time)
 images = glob.glob('*MFS-image-pb.fits')
 images.sort()
-ref_image = images[0]
-print images
 
+#Use first image as reference image
+ref_image = images[0]
+
+
+#For each image either copy (if reference image) or 
+#run mirRegrid to regrid image to reference
 for i in range(len(images)):
 	if i > 0:
 		img = images[i]
 		path = os.getcwd()
 		cmd = './mirRegrid %s %s' %(ref_image[:-5], img[:-5])
-		print cmd
 		os.system(cmd)
-		#out_name = regridIm(images[i], ref_image)
-		#print out_name
 	else:
 		new_img = ref_image[:-5]+'.regrid.fits'
 		cmd = 'cp %s %s' %(ref_image, new_img)
 		os.system(cmd)
+
+##Clean up directory
+os.system('rm -rf *.in')
+os.system('rm -rf *regridded*')
 
 
 
